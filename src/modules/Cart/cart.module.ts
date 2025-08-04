cf9ff4f37dbec44179c2d8cd63ac74e5c5cd6ec3
Nour Eid <noorhamdyeid19@gmail.com>
2025-08-04 10:53:44 +0300

    feat: implement CartService with methods to get and create user carts

--- src/modules/Cart/cart.module.ts
+++ src/modules/Cart/cart.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cart } from './cart.entity';
 import { CartItem } from './cart-item.entity';
+import { CartService } from './cart.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Cart, CartItem])],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [CartService],
+  exports: [CartService],
 })
 export class CartModule {}
---
+++ src/modules/Cart/cart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Cart } from './cart.entity';
+import { CartItem } from './cart-item.entity';
+
+@Injectable()
+export class CartService {
+  constructor(
+    private readonly cartRepository: Repository<Cart>,
+    private readonly cartItemRepository: Repository<CartItem>,
+  ) {}
+
+  async getUserCart(userId: string): Promise<Cart | null> {
+    return this.cartRepository.findOne({
+      where: { userId },
+      relations: ['cartItems', 'cartItems.product'],
+    });
+  }
+
+  async createCart(userId: string): Promise<Cart> {
+    const cart = this.cartRepository.create({ userId });
+    return this.cartRepository.save(cart);
+  }
+}
