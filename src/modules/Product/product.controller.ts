8c49c7dc0fb63bba6da3544405edd4f9e1daec40
Nour Eid <noorhamdyeid19@gmail.com>
2025-08-15 10:25:33 +0300

    feat: enhance product retrieval with filtering and pagination in ProductController

--- src/modules/Product/product.controller.ts
+++ src/modules/Product/product.controller.ts
@@ -4,12 +4,16 @@ import {
   Controller,
   Get,
   Param,
+  ParseFloatPipe,
+  ParseIntPipe,
   ParseUUIDPipe,
   Post,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { CategoryService } from '../Category/category.service';
+import { Query } from '@nestjs/common';
+import { Between, Like } from 'typeorm';
 
 @Controller('products')
 export class ProductController {
@@ -19,8 +23,24 @@ export class ProductController {
   ) {}
 
   @Get()
-  async findAll() {
-    return this.productService.findAll({ relations: ['category'] });
+  async findAll(
+    @Query('categoryId') categoryId?: string,
+    @Query('search') search?: string,
+    @Query('minPrice', new ParseFloatPipe()) minPrice = 0,
+    @Query('maxPrice', new ParseFloatPipe()) maxPrice = 1_000_0000,
+    @Query('page', new ParseIntPipe()) page = 1,
+  ) {
+    return this.productService.findAll({
+      relations: {
+        category: true,
+      },
+      where: {
+        price: Between(minPrice, maxPrice),
+        ...(categoryId && { category: { id: categoryId } }),
+        ...(search && { search: Like(`%${search}%`) }),
+      },
+      take: page,
+    });
   }
 
   @Get(':id')
